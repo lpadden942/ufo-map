02bc0974a32c2470308137f12fb631848309bb48
lpadden942 <lpadden942@gmail.com>
2020-06-10 16:39:15 -0400

    added script tag for geolocation api, implemented asking the user for permission to access their location on page load

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ function initMap() {
         zoom: 8
     });
 }
+
+if (navigator.geolocation) {
+    window.onload = function() {
+        let geoSuccess = function(position) {
+           map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+        };
+        let geoError = function(error) {
+            console.log("Error occurred. Error code: " + error.code);
+        };
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+}
+
