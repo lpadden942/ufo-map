66717ea74b87de42471ac407f8397fee967202aa
lpadden942 <lpadden942@gmail.com>
2020-06-12 15:07:05 -0400

    added json file with sightings, placed marker on map for sighting

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 let map;
+let sightings = [{
+    "datetime": "10/11/2008 19:00",
+    "city": "bridgeport",
+    "state": "ct",
+    "country": "us",
+    "shape": "circle",
+    "duration (seconds)": 7200,
+    "duration (hours/min)": "2 hours",
+    "comments": "I saw 6 glowing orbs lining up in different formations witin a 2 hour period over Long Island Sound.",
+    "date posted": "10/31/2008",
+    "latitude": 41.1669444,
+    "longitude": -73.2052778
+}];
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -34.397, lng: 150.644 },
         zoom: 8
     });
+    for (let i = 0; i < sightings.length; i++) {
+        
+        let marker = new google.maps.Marker({
+            position: {lat: parseFloat(sightings[i].latitude), lng: parseFloat(sightings[i].longitude)},
+            title: sightings[i].city + " " + sightings[i].state + " " + sightings[i].shape
+        });
+        marker.setMap(map);
+    }
 }
 
 if (navigator.geolocation) {
@@ -18,4 +39,3 @@ if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
     }
 }
-
